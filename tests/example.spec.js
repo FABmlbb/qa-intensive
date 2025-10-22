bedb1ebd4374a271e60e7de5ea2215507d3f5c86
Анастасия <anastasia@MacBook-Pro-Anastasia.local>
2025-10-22 18:45:31 +0300

    chore: remove example test

--- tests/example.spec.js
+++
@@ -1,6 +0,0 @@
-const { test, expect } = require('@playwright/test');
-
-test('Проверка заголовка страницы', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  await expect(page).toHaveTitle(/Playwright/);
-});
